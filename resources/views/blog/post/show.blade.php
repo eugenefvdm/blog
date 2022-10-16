f805288772b9733804176b24e9e73b4835eb7714
Eugene <eugenevdm@gmail.com>
2022-10-16 10:22:29 +0200

    decrease padding on blog show page

--- resources/views/blog/post/show.blade.php
+++ resources/views/blog/post/show.blade.php
@@ -2,7 +2,7 @@
 
     @include('layouts.breadcrumbs')
 
-    <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900">
+    <main class="pt-6 pb-16 lg:pt-12 lg:pb-24 bg-white dark:bg-gray-900">
         <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
             <article
                 class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
