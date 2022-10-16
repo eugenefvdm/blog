65653137f6ce6e7e783b3112b12cc2fb8e1fe4cb
Eugene <eugenevdm@gmail.com>
2022-10-16 14:48:45 +0200

    access permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,6 +66,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessFilament(): bool
     {
-        return str_ends_with($this->email, '@example.com');
+        return str_ends_with($this->email, env('FILAMENT_ADMIN_DOMAIN', '@example.com'));
     }
 }
