fb3379211a9c7ebdb4819192a6740206af6e8bbe
Eugene <eugenevdm@gmail.com>
2023-03-11 09:55:15 +0200

    fix logo width and height

--- resources/views/vendor/jetstream/components/application-mark.blade.php
+++ resources/views/vendor/jetstream/components/application-mark.blade.php
@@ -1,2 +1,3 @@
 {{-- <img class="h-9 w-auto" src='/img/logo.png'> --}}
-<img class="h-9 w-auto" src='/img/logo.webp' alt='brand logo'>
+{{-- <img class="h-9 w-auto" src='/img/logo.webp' alt='brand logo' width='32' height='32'> --}}
+<img src='/img/logo.webp' alt='brand logo' width='32' height='32'>
