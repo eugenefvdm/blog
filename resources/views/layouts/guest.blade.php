f7e6eacd431591d3062aaccf7795972a46f33cef
Eugene <eugenevdm@gmail.com>
2023-03-12 10:44:05 +0200

    implement a preload for the fonts file

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,11 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        {{-- <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap"> --}}
+        <link rel="preload" 
+            href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap"
+            as="style"
+            onload="this.onload=null; this.rel='stylesheet'; document.body.classList.add('fontLoaded')">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
